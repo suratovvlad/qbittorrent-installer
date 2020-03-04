0424112471dec90a2876b757b854d4754939724f
suratovvlad <suratovvlad@gmail.com>
2020-03-05 02:38:09 +0300

    Stable 4.2.1 release

--- build_installer_mingw64.sh
+++ build_installer_mingw64.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #/d/Qt/QtIFW-3.1.1/bin/binarycreator.exe -c config/config.xml -p packages/ -f qBittorrentDarkInstaller.exe -r config/style.qrc
-#binarycreator -c config/config.xml -p packages/ -f qBittorrentDarkInstaller.exe
+binarycreator -c config/config.xml -p packages/ -f qBittorrentDarkInstaller.exe
 #/d/Qt/QtIFW-3.1.1/bin/binarycreator.exe -c config/config.xml -p packages/ -f qBittorrentDarkInstaller.exe 
-/c/Users/surat/Documents/GitHub/qr-installer-framework/pkg/mingw-w64-x86_64-qt-installer-framework/mingw64/bin/binarycreator -c config/config.xml -p packages/ -f qBittorrentDarkInstaller.exe
+#/c/Users/surat/Documents/GitHub/qr-installer-framework/pkg/mingw-w64-x86_64-qt-installer-framework/mingw64/bin/binarycreator -c config/config.xml -p packages/ -f qBittorrentDarkInstaller.exe
